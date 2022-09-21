9f03a4cb58b3bfc5e263bae8aa623b54528f4436
Amy F Schaeffer <65150396+afschaeffer@users.noreply.github.com>
2022-09-20 23:42:09 -0700

    v2
    This version has been updated to accommodate work around for large electric files, gathering them from the LLE Extractor excel sheet. Gathering of drawing titles functionality removed to meet deployment deadline due to pdf point issues. Additional functions include printing required drawings to data details sheet. Projects still needs to do the following: 1) Find page numbers from page  data for drawing numbers in required data and put page numbers into dictionary as a list pair with LLE package title as the key. 2) For each LLE Package Title key, extract page numbers in values and save them as their own PDF file with LLE Package title as the name of the pdf. 3) I need to connect macro button on LLE Extractor excel workbook to run the python file.

--- LLE-Extraction.py
+++ LLE-Extraction.py
@@ -5,7 +5,7 @@
 #
 # Version: 1
 # Start Date:        8/30/2022
-# End Date:          9/14/2022
+# End Date:          9/22/2022
 # Description: Identifies required drawing numbers from a range in a user defined Excel 
 #              sheet and parses through a Drawing Set PDF, extracting related
 #              pages and compiling them into a new PDF file. Additionally, prints list of extracted
@@ -14,6 +14,8 @@
 #----------------------------------------------------------------------------------------------------
 
 # Importing xlwings to read and print in Excel
+from ast import Num
+from numbers import Number
 import xlwings as xw
 # Importing PDFMiner Libraries
 from pdfminer.layout import LAParams
@@ -21,7 +23,6 @@ from pdfminer.pdfpage import PDFPage
 from pdfminer.pdfinterp import PDFPageInterpreter, PDFResourceManager
 from pdfminer.converter import PDFPageAggregator
 
-# print dictionary from package_req into LlE Extraction details sheet 
 
 # Search for instance of needed drawing numbers in PDF data dictionary, creating new list of key value pairs with data (drawing number from excel file: page number from parsed data)
 # If drawing name required is not found in parsed PDF, add drawing name to new list and
@@ -31,15 +32,53 @@ from pdfminer.converter import PDFPageAggregator
 
 # Save new PDF file to specified path
 
-# Function to add key if it doesnt already exist, and add list as pair value to key in a dictionary
-def add_values_in_dict(sample_dict, key, list_of_values):
-    if key not in sample_dict:
-        sample_dict[key] = list()
-    sample_dict[key].extend(list_of_values)
-    return sample_dict
+# Function to add passed key and value to passed dictionary
+def add_values_in_dict(sample_dict, key, value):
+    sample_dict[key] = value
+    return sample_dict 
+
+# Function that formats drawing numbers to get rid of any extra characters at the end
+def format_drawing_number(drawing_number):
+    count = 0
+    for i in range(len(drawing_number)):
+        char = drawing_number[i]
+        if char == "0" or char == "1" or char == "2" or char == "3" or char == "4" or char == "5" or char == "6" or char == "7" or char == "8" or char == "9":
+            count = count + 1
+        if count == 3:
+            drawing_number = drawing_number[0:i+1:]
+            break
+    return drawing_number
+
+# Function to format page number from drawing log to page number only
+def format_e_page_num(page_num):
+    count = 0
+    for i in range(len(page_num)):
+        char = page_num[i]
+        if char == " ":
+            count = count + 1
+        if count == 2:
+            page_num = page_num[5:i]
+            break
+    return page_num
+
+#Function to collect electrical data from the LLE Extractor file
+def get_e_drawing_data(electrical_drawing_sheet):
+    ret = {}
+    # Looping through range between rows 4 and 500 for formatting purpose
+    for row in range(4,500):
+        # Adding values in columns A and B to dictionary until it hits a blank cell
+        cell_value = electrical_drawing_sheet[f'A{row}'].value
+        if cell_value != None:
+            key = electrical_drawing_sheet[f'A{row}'].value
+            value = electrical_drawing_sheet[f'B{row}'].value
+            value = format_e_page_num(value)
+            ret[key] = value
+        else:
+            break
+    return ret
 
 # Function parsing passed drawing set PDF, gathering drawing number and title from passed related coordinates, incrementing page number and storing and returning data in a dictionary
-def get_drawing_set_data(drawing_set, drawing_title_xmin, drawing_title_ymin, drawing_title_ymax, drawing_number_xmin, drawing_number_ymax):
+def get_drawing_set_data(drawing_set, drawing_number_xmin, drawing_number_xmax, drawing_number_ymin, drawing_number_ymax):
     # Opening PDF and creating parser object
     fp = open(drawing_set, 'rb')
     manager = PDFResourceManager()
@@ -52,29 +91,25 @@ def get_drawing_set_data(drawing_set, drawing_title_xmin, drawing_title_ymin, dr
     page_num = 0
     page_data = {}
     
-    #Looping through each page, parsing it, and collecting and storing drawing set, number, and page data
+    #Looping through each page, parsing it, and collecting and storing drawing number and page data
     for page in pages:
         interpreter.process_page(page)
         layout = dev.get_result()
 
         for lobj in layout: 
             # Checking if LTTextbox is within drawing number coordinates, storing text if true
-            if (lobj.bbox[0] == drawing_number_xmin) and (lobj.bbox[3] == drawing_number_ymax):
-                drawing_number = lobj.get_text()
-                #removing additional spaces in front of and at end of string
-                drawing_number = drawing_number.strip()
-            try:
-                # checking if LTTextbox is within drawing title coordinates, storing text if true
-                if (lobj.bbox[0] >= drawing_title_xmin) and (lobj.bbox[1] >= drawing_title_ymin) and (lobj.bbox[3] <= drawing_title_ymax):
-                    drawing_title = lobj.get_text()
+             if (lobj.bbox[0] >= drawing_number_xmin) and (lobj.bbox[1] >= drawing_number_ymin) and (lobj.bbox[2] <= drawing_number_xmax) and (lobj.bbox[3] <= drawing_number_ymax):
+                try:
+                    drawing_number = lobj.get_text()
                     #removing additional spaces in front of and at end of string
-                    drawing_title = drawing_title.strip()
-            except:
-                pass
+                    drawing_number = drawing_number.strip()
+                    #drawing_number = format_drawing_number(drawing_number)
+                except:
+                    continue
         # Incrementing page number
         page_num = page_num + 1
         # Adding page values into dictionary, with drawing number as the key and list with drawing title and page number as the value pair
-        add_values_in_dict(page_data, drawing_number, [drawing_title,page_num])
+        add_values_in_dict(page_data, drawing_number, page_num)
     # Returning data in a dictionary
     return page_data
 
@@ -86,7 +121,6 @@ def print_detailed_data(destination_sheet, dictionary):
     #clearing contents of columns prior to printing
     destination_sheet.range('A4:A500').clear_contents()
     destination_sheet.range('B4:B500').clear_contents()
-    destination_sheet.range('C4:C500').clear_contents()
     # printing each key value into column A in sheet, incrementing counter after each iteration
     for key, value in dictionary.items():
         destination_sheet.range(f'A{key_counter}').options(index=False).value = key
@@ -96,27 +130,12 @@ def print_detailed_data(destination_sheet, dictionary):
         destination_sheet.range(f'B{value_counter}').options(index=False).value = value
         value_counter = value_counter +1  
 
-# Function that formats drawing numbers to get rid of any extra characters at the end, called in get_package_req
-def format_drawing_number(drawing_number):
-    index = 0
-    count = 0
-    for i in range(len(drawing_number)):
-        char = drawing_number[i]
-        if char == "0" or char == "1" or char == "2" or char == "3" or char == "4" or char == "5" or char == "6" or char == "7" or char == "8" or char == "9":
-            count = count + 1
-        if count > 3:
-            drawing_number = drawing_number[0:i:]
-            break
-    return drawing_number
-
 # Function gathering drawing numbers in B column associated with X values for selected package type
 def get_package_req(pac_sheet, package_type, m_row_min, m_row_max, p_row_min,  p_row_max, e_row_min, e_row_max):
     # Dictionary to contain required list of drawing numbers as value and package category from row 20 as key 
     req_drawings = {}
     # Indexing column letters as key value pairs to loop through (excel uses [1] index for column letters)
     col_names = {27:"AA",28:"AB",29:"AC",30:"AD",31:"AE",32:"AF",33:"AG",34:"AH",35:"AI",36:"AJ",37:"AK",38:"AL",   39:"AM",40:"AN",41:"AO",42:"AP",43:"AQ",44:"AR",45:"AS"}
-    # starting counter for looping through columns at 27 for row AA
-    col_counter = 27
 
     # Nested function taking row range parameter to only gather drawing numbers for matching package type
     def req_drawings_for_package_type(rows):
@@ -156,14 +175,32 @@ def get_package_req(pac_sheet, package_type, m_row_min, m_row_max, p_row_min,  p
     # Returning dictionary with key value being package category and value being list of drawing numbers, only for package categories with drawing numbers marked "x" for required in their column and range
     return req_drawings 
 
+# Function to print required drawings to excel
+def print_req_drawings(sheet, dictionary):
+    # Creating counters for printing data into cells in each column
+    key_counter = 4
+    value_counter = 4
+    # Dictionary holding column indexes that will need cleared prior to printing to excel
+    col_names = {4:"D",5:"E",6:"F",7:"G",8:"H",9:"I",10:"J",11:"K",12:"L",13:"M",14:"N",15:"O",16:"P",17:"Q",18:"R",19:"S",20:"T",21:"U",22:"V",23:"W",24:"X",25:"Y",26:"Z",27:"AA",28:"AB",29:"AC",30:"AD",31:"AE",32:"AF",33:"AG"}
+    # Clearing contents of columns prior to printing
+    for num, col in col_names.items():
+        sheet.range(f'{col}4:{col}20').clear_contents()
+    # Printing each key value into column D in sheet, incrementing counter after each iteration
+    for key, value in dictionary.items():
+        sheet.range(f'D{key_counter}').options(index=False).value = key
+        key_counter = key_counter +1
+    #printing each value from list into columns E+, incrementing counter after each iteration
+    for values, value in dictionary.items():
+        sheet.range(f'E{value_counter}').options(index=False).value = value
+        value_counter = value_counter +1  
+
 # Main Script Logic -------------------------------------------------------
 
 # PDF Pixel coordinate values for drawing title and drawing number, for passing to get_drawing_set_data method
-drawing_title_xmin = 2727
-drawing_title_ymin = 95
-drawing_title_ymax = 145
-drawing_number_xmin = 2829.4199
-drawing_number_ymax = 125.71413779999986
+drawing_number_xmin = 2740
+drawing_number_ymin = 70
+drawing_number_xmax = 2963
+drawing_number_ymax = 96
 
 # Get user inputs - Excel sheet with drawing numbers, drawing set, new file name.
 # Assigning required variables for functions to sheet names, document names, cell ranges, etc
@@ -174,6 +211,7 @@ worksheet = xw.sheets
 # Defining sheets in extractor workbook
 home_sheet = extractor_wb.sheets['Home']
 data_sheet = extractor_wb.sheets['Detailed Data']
+electrical_sheet = extractor_wb.sheets['Electrical Drawing Data']
 #Reading user inputs (Excel sheet, PDF drawings, and name for the output file) and assigning them variable names 
 design_deliverables = home_sheet.range('B7').value
 drawing_set = home_sheet.range('B10').value
@@ -191,14 +229,20 @@ p_row_max = 93
 e_row_min = 95
 e_row_max = 119
 
-# Calling function to get data from drawing set, assigning dictionary to variable name page_data
-page_data = get_drawing_set_data(drawing_set, drawing_title_xmin, drawing_title_ymin, drawing_title_ymax, drawing_number_xmin, drawing_number_ymax)
-
-# Calling function to paste all extracted page details into Detailed Data sheet for review
+# Calling functions to gather page data. Determining how to gather page data using conditional statements and assigning variable to returned dictionary
+if package_type == "Electrical":
+    # Calling function to collect electrical data from excel sheet
+    page_data = get_e_drawing_data(electrical_sheet)
+elif package_type == "Mechanical" or package_type == "Plumbing":
+    # Calling function to get data from drawing set, assigning dictionary to variable name page_data
+    page_data = get_drawing_set_data(drawing_set, drawing_number_xmin, drawing_number_xmax, drawing_number_ymin, drawing_number_ymax)
+    
+# Calling function to print all extracted page details into Detailed Data sheet for review
 print_detailed_data(data_sheet, page_data)
 
-# Calling function to return dictionary with key value being package category (row 20 in PAC sheet) and value being list of required drawings
+#Calling function to return dictionary with key value being package category (row 20 in PAC sheet) and value being list of required drawings
 package_req = get_package_req(pac_sheet, package_type, m_row_min, m_row_max, p_row_min,  p_row_max, e_row_min, e_row_max)
-print(package_req)
 
+#Calling function to print required drawings to excel
+print_req_drawings(data_sheet, package_req)
 
