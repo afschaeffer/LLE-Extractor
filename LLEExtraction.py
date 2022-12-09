a06628f7d2f9058349502480000b8ee3c9df519f
Amy F Schaeffer <65150396+afschaeffer@users.noreply.github.com>
2022-12-08 22:29:53 -0800

    Add files via upload
    v3

--- LLEExtraction.py
+++ LLEExtraction.py
@@ -3,25 +3,22 @@
 # Script Name: LLE-Extraction.py
 # Author:      Amy Schaeffer
 #
-# Version: 2
+# Version: 3
 # Start Date:        9/20/2022
 # End Date:          9/22/2022
 # Description: Identifies required drawing numbers from a range in a user defined Excel 
 #              sheet and parses through a Drawing Set PDF, extracting related
 #              pages and compiling them into a new PDF file. Additionally, prints list of extracted
 #              page data into separate sheet in excel for further review if needed 
-# 
+
+# Version 3: Updated - Removed function paring for the drawing set data due to it increasing runtime in 
+#            leiu of manually entering data.
 #----------------------------------------------------------------------------------------------------
 
 # Importing xlwings to read and print in Excel
 import xlwings as xw
 #Importing Path to create full paths for files
 from pathlib import Path
-# Importing PDFMiner Libraries
-from pdfminer.layout import LAParams
-from pdfminer.pdfpage import PDFPage
-from pdfminer.pdfinterp import PDFPageInterpreter, PDFResourceManager
-from pdfminer.converter import PDFPageAggregator
 #Importing PyPDF2 to split pdf into LLE packages
 from PyPDF2 import PdfFileReader, PdfFileWriter
 #Importing os
@@ -66,57 +63,21 @@ def format_e_page_num(page_num):
     return page_num
 
 #Function to collect electrical data from the LLE Extractor file
-def get_e_drawing_data(electrical_drawing_sheet):
+def get_drawing_data(drawing_sheet):
     ret = {}
     # Looping through range between rows 4 and 500 for formatting purpose
     for row in range(4,500):
         # Adding values in columns A and B to dictionary until it hits a blank cell
-        cell_value = electrical_drawing_sheet[f'A{row}'].value
+        cell_value = drawing_sheet[f'A{row}'].value
         if cell_value != None:
-            key = electrical_drawing_sheet[f'A{row}'].value
-            value = electrical_drawing_sheet[f'B{row}'].value
+            key = drawing_sheet[f'A{row}'].value
+            value = drawing_sheet[f'B{row}'].value
             value = format_e_page_num(value)
             value = int(value)
             ret[key] = value
         else:
             break
     return ret
-
-# Function parsing passed drawing set PDF, gathering drawing number and title from passed related coordinates, incrementing page number and storing and returning data in a dictionary
-def get_drawing_set_data(drawing_set, drawing_number_xmin, drawing_number_xmax, drawing_number_ymin, drawing_number_ymax):
-    # Opening PDF and creating parser object
-    fp = open(drawing_set, 'rb')
-    manager = PDFResourceManager()
-    laparams = LAParams()
-    dev = PDFPageAggregator(manager, laparams=laparams)
-    interpreter = PDFPageInterpreter(manager, dev)
-    pages = PDFPage.get_pages(fp)
-    
-    # Creating empty page number variable and page data dict
-    page_num = 0
-    page_data = {}
-    
-    #Looping through each page, parsing it, and collecting and storing drawing number and page data
-    for page in pages:
-        interpreter.process_page(page)
-        layout = dev.get_result()
-
-        for lobj in layout: 
-            # Checking if LTTextbox is within drawing number coordinates, storing text if true
-             if (lobj.bbox[0] >= drawing_number_xmin) and (lobj.bbox[1] >= drawing_number_ymin) and (lobj.bbox[2] <= drawing_number_xmax) and (lobj.bbox[3] <= drawing_number_ymax):
-                try:
-                    drawing_number = lobj.get_text()
-                    #removing additional spaces in front of and at end of string
-                    drawing_number = drawing_number.strip()
-                    #drawing_number = format_drawing_number(drawing_number)
-                except:
-                    continue
-        # Incrementing page number
-        page_num = page_num + 1
-        # Adding page values into dictionary, with drawing number as the key and list with drawing title and page number as the value pair
-        add_values_in_dict(page_data, drawing_number, page_num)
-    # Returning data in a dictionary
-    return page_data
 
 # Function to update Data Details sheet with all key value pairs found in page data dictionary
 def print_detailed_data(destination_sheet, dictionary):
@@ -137,7 +98,7 @@ def get_package_req(pac_sheet, package_type, m_row_min, m_row_max, p_row_min,  p
     # Dictionary to contain required list of drawing numbers as value and package category from row 20 as key 
     req_drawings = {}
     # Indexing column letters as key value pairs to loop through (excel uses [1] index for column letters)
-    col_names = {27:"AA",28:"AB",29:"AC",30:"AD",31:"AE",32:"AF",33:"AG",34:"AH",35:"AI",36:"AJ",37:"AK",38:"AL",   39:"AM",40:"AN",41:"AO",42:"AP",43:"AQ",44:"AR",45:"AS"}
+    col_names = {27:"AA",28:"AB",29:"AC",30:"AD",31:"AE",32:"AF",33:"AG",34:"AH",35:"AI",36:"AJ",37:"AK",38:"AL",39:"AM",40:"AN",41:"AO",42:"AP",43:"AQ",44:"AR",45:"AS"}
 
     # Nested function taking row range parameter to only gather drawing numbers for matching package type
     def req_drawings_for_package_type(rows):
@@ -254,10 +215,12 @@ def store_files(cwd, page_nums, folder_name):
 
 # Function to clear areas on LLE Extractor excel sheet in the most chaotic way possible, im sorry
 def clear_sheet():
-    extractor_wb = xw.Book('LLE-Extractor-v1.0.xlsm')
+    extractor_wb = xw.Book('LLE-Extractor-v3.0.xlsm')
     home_sheet = extractor_wb.sheets['Home']
     data_sheet = extractor_wb.sheets['Detailed Data']
     electrical_sheet = extractor_wb.sheets['Electrical Drawing Data']
+    mechanical_sheet = extractor_wb.sheets['Mechanical Drawing Data']
+    plumbing_sheet = extractor_wb.sheets['Plumbing Drawing Data']
 
     col_names = {4:"D",5:"E",6:"F",7:"G",8:"H",9:"I",10:"J",11:"K",12:"L",13:"M",14:"N",15:"O",16:"P",17:"Q",18:"R",19:"S",20:"T",21:"U",22:"V",23:"W",24:"X",25:"Y",26:"Z",27:"AA",28:"AB",29:"AC",30:"AD",31:"AE",32:"AF",33:"AG"}
     for num, col in col_names.items():
@@ -272,55 +235,68 @@ def clear_sheet():
     home_sheet.range('B10').merge_area.clear_contents()
     home_sheet.range('B13').merge_area.clear_contents()
     home_sheet.range('C15').clear_contents()
+    home_sheet.range('F15').clear_contents()
     electrical_sheet.range('A4:A500').clear_contents()
     electrical_sheet.range('B4:B500').clear_contents()
+    mechanical_sheet.range('A4:A500').clear_contents()
+    mechanical_sheet.range('B4:B500').clear_contents()
+    plumbing_sheet.range('A4:A500').clear_contents()
+    plumbing_sheet.range('B4:B500').clear_contents()
 
 # Main Script Logic -------------------------------------------------------
 def run_LLE():
-    # PDF Pixel coordinate values for drawing title and drawing number, for passing to get_drawing_set_data method
-    drawing_number_xmin = 2740
-    drawing_number_ymin = 70
-    drawing_number_xmax = 2963
-    drawing_number_ymax = 96
 
     # Defining current directory where program is running from
     cwd = r"Z:\Global\0.0 - Administrative - Management\Document Control\Tools\LLE Extraction Tool"
     # Get user inputs - Excel sheet with drawing numbers, drawing set, new file name.
     # Assigning required variables for functions to sheet names, document names, cell ranges, etc
     #Opening Extractor Workbook
-    extractor_wb = xw.Book(rf'{cwd}\LLE-Extractor-v1.0.xlsm')
+    extractor_wb = xw.Book(rf'{cwd}\LLE-Extractor-v3.0.xlsm')
     #Viewing worksheets available
     #worksheet = xw.sheets
     # Defining sheets in extractor workbook
     home_sheet = extractor_wb.sheets['Home']
     data_sheet = extractor_wb.sheets['Detailed Data']
     electrical_sheet = extractor_wb.sheets['Electrical Drawing Data']
+    mechanical_sheet = extractor_wb.sheets['Mechanical Drawing Data']
+    plumbing_sheet = extractor_wb.sheets['Plumbing Drawing Data']
     #Reading user inputs (Excel sheet, PDF drawings, and name for the output file) and assigning them variable names 
-    design_deliverables = home_sheet.range('B7').value
-    design_deliverables = rf"{cwd}\{design_deliverables}"
+    register = home_sheet.range('B7').value
+    register = rf"{cwd}\{register}"
     drawing_set = home_sheet.range('B10').value
     drawing_set = rf"{cwd}\{drawing_set}"
     folder_name = home_sheet.range('B13').value
     package_type = home_sheet.range('C15').value
+    stories = home_sheet.range('F15').value
     # Opening Design Deliverables Workbook
-    design_deliverables_wb = xw.Book(design_deliverables)
+    register_wb = xw.Book(register)
     # Defining sheets in design deliverables workbook
-    pac_sheet = design_deliverables_wb.sheets['PAC']
-    # Defining minimum and maximum row numbers for searching for x values, by package type
-    m_row_min = 26
-    m_row_max = 68
-    p_row_min = 70
-    p_row_max = 93
-    e_row_min = 95
-    e_row_max = 119
+    pac_sheet = register_wb.sheets['PAC-DWG']
+   # Defining minimum and maximum row numbers for searching for x values, by package type
+    if stories == "1s":
+        m_row_min = 26
+        m_row_max = 58
+        p_row_min = 60
+        p_row_max = 83
+        e_row_min = 85
+        e_row_max = 109
+    elif stories == "2s":
+        m_row_min = 26
+        m_row_max = 61
+        p_row_min = 63
+        p_row_max = 94
+        e_row_min = 96
+        e_row_max = 126
 
     # Calling functions to gather page data. Determining how to gather page data using conditional statements and assigning variable to returned dictionary
     if package_type == "Electrical":
         # Calling function to collect electrical data from excel sheet
-        page_data = get_e_drawing_data(electrical_sheet)
-    elif package_type == "Mechanical" or package_type == "Plumbing":
+        page_data = get_drawing_data(electrical_sheet)
+    elif package_type == "Mechanical":
+        page_data = get_drawing_data(mechanical_sheet)
+    elif package_type == "Plumbing":
         # Calling function to get data from drawing set, assigning dictionary to variable name page_data
-        page_data = get_drawing_set_data(drawing_set, drawing_number_xmin, drawing_number_xmax, drawing_number_ymin, drawing_number_ymax)
+        page_data = get_drawing_data(plumbing_sheet)
         
     # Calling function to print all extracted page details into Detailed Data sheet for review
     print_detailed_data(data_sheet, page_data)
@@ -340,49 +316,58 @@ def run_LLE():
 
 # Function only gathering and displaying data so user can fix errors without building a new LLE package each time
 def data_only():
-     # PDF Pixel coordinate values for drawing title and drawing number, for passing to get_drawing_set_data method
-    drawing_number_xmin = 2740
-    drawing_number_ymin = 70
-    drawing_number_xmax = 2963
-    drawing_number_ymax = 96
 
     # Defining current directory where program is running from
     cwd = r"Z:\Global\0.0 - Administrative - Management\Document Control\Tools\LLE Extraction Tool"
     # Get user inputs - Excel sheet with drawing numbers, drawing set, new file name.
     # Assigning required variables for functions to sheet names, document names, cell ranges, etc
     #Opening Extractor Workbook
-    extractor_wb = xw.Book(rf'{cwd}\LLE-Extractor-v1.0.xlsm')
+    extractor_wb = xw.Book(rf'{cwd}\LLE-Extractor-v3.0.xlsm')
     #Viewing worksheets available
     #worksheet = xw.sheets
     # Defining sheets in extractor workbook
     home_sheet = extractor_wb.sheets['Home']
     data_sheet = extractor_wb.sheets['Detailed Data']
     electrical_sheet = extractor_wb.sheets['Electrical Drawing Data']
-    #Reading user inputs (Excel sheet, PDF drawings, and name for the output file) and assigning them variable names 
-    design_deliverables = home_sheet.range('B7').value
-    design_deliverables = rf"{cwd}\{design_deliverables}"
+    mechanical_sheet = extractor_wb.sheets['Mechanical Drawing Data']
+    plumbing_sheet = extractor_wb.sheets['Plumbing Drawing Data']
+   #Reading user inputs (Excel sheet, PDF drawings, and name for the output file) and assigning them variable names 
+    register = home_sheet.range('B7').value
+    register = rf"{cwd}\{register}"
     drawing_set = home_sheet.range('B10').value
     drawing_set = rf"{cwd}\{drawing_set}"
     package_type = home_sheet.range('C15').value
+    stories = home_sheet.range('F15').value
     # Opening Design Deliverables Workbook
-    design_deliverables_wb = xw.Book(design_deliverables)
+    # Opening Design Deliverables Workbook
+    register_wb = xw.Book(register)
     # Defining sheets in design deliverables workbook
-    pac_sheet = design_deliverables_wb.sheets['PAC']
+    pac_sheet = register_wb.sheets['PAC-DWG']
     # Defining minimum and maximum row numbers for searching for x values, by package type
-    m_row_min = 26
-    m_row_max = 68
-    p_row_min = 70
-    p_row_max = 93
-    e_row_min = 95
-    e_row_max = 119
+    if stories == "1s":
+        m_row_min = 26
+        m_row_max = 58
+        p_row_min = 60
+        p_row_max = 83
+        e_row_min = 85
+        e_row_max = 109
+    elif stories == "2s":
+        m_row_min = 26
+        m_row_max = 61
+        p_row_min = 63
+        p_row_max = 94
+        e_row_min = 96
+        e_row_max = 126
 
-    # Calling functions to gather page data. Determining how to gather page data using conditional statements and assigning variable to returned dictionary
+   # Calling functions to gather page data. Determining how to gather page data using conditional statements and assigning variable to returned dictionary
     if package_type == "Electrical":
         # Calling function to collect electrical data from excel sheet
-        page_data = get_e_drawing_data(electrical_sheet)
-    elif package_type == "Mechanical" or package_type == "Plumbing":
+        page_data = get_drawing_data(electrical_sheet)
+    elif package_type == "Mechanical":
+        page_data = get_drawing_data(mechanical_sheet)
+    elif package_type == "Plumbing":
         # Calling function to get data from drawing set, assigning dictionary to variable name page_data
-        page_data = get_drawing_set_data(drawing_set, drawing_number_xmin, drawing_number_xmax, drawing_number_ymin, drawing_number_ymax)
+        page_data = get_drawing_data(plumbing_sheet)
         
     # Calling function to print all extracted page details into Detailed Data sheet for review
     print_detailed_data(data_sheet, page_data)
